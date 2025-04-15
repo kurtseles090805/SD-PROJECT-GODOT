1fd278c027a6495d4db4d33b844f7989c485c209
Razelian <148513002+Razelian@users.noreply.github.com>
2025-04-16 00:33:40 +0800

    working apk

--- scenes/final game scene diff approach (not working)/scripts/bulb_component.gd
+++
@@ -1,8 +0,0 @@
-extends BaseComponent
-class_name BulbComponent
-
-#@export var resistance := 500.0  # Example resistance value for a bulb
-
-func _ready():
-	component_type = "bulb"
-	# Add any bulb-specific initialization here
